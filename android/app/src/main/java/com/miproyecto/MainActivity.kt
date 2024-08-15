716230e055125abbe95b1e1292977a66f5ce6949
iamBrunoL <141052273+iamBrunoL@users.noreply.github.com>
2024-08-15 11:59:14 -0600

    ajustes para el uso del logo y renombrar la aplicacion

--- android/app/src/main/java/com/miproyecto/MainActivity.kt
+++ android/app/src/main/java/com/miproyecto/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.miproyecto
+package com.cinefox
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "MiProyecto"
+  override fun getMainComponentName(): String = "CineFox"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/miproyecto/MainApplication.kt
+++ android/app/src/main/java/com/miproyecto/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.miproyecto
+package com.cinefox
 
 import android.app.Application
 import com.facebook.react.PackageList
